7472c30c67a8d34bfe44b8b6229e744c0222c957
Alex007-Biz <bizukin@yandex.ru>
2024-04-03 23:34:00 +0300

    4

--- two.py
+++ two.py
@@ -7,11 +7,14 @@ class Teacher():
 class School():
     def __init__(self, new_teacher):
         self.teacher = new_teacher
-    def start_lesson():
-        self.teacher.teach()
+    #def start_lesson():
+     #   teacher.teach()
 
 my_teacher = Teacher("John")
 my_teacher.teach()
+
+new_school = School(my_teacher)
+#new_school.start_lesson()
 
 #name = School(my_teacher)
 #def __init__(self, new_teacher):
